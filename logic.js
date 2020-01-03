1885b105ab8be19c7d0e8916baf0cad392f0d909
Chad Kaplan <chadkaplan@knights.ucf.edu>
2020-01-03 00:25:40 -0500

    Solved the problem trying to remove the first item in an array and add an item to the end

--- logic.js
+++ logic.js
@@ -5,3 +5,29 @@ const uncensor = (str, vowels) => {
     const arr = vowels.split('');
     return str.replace(/\*/g, () => arr.shift());
   };
+
+//   =======================================================================================
+
+// Write a function that takes an array and a number as arguments. Add the number to the end of the array, then remove the first element of the array. The function should then return the updated array.
+
+// nextInLine([5, 6, 7, 8, 9], 1) ➞ [6, 7, 8, 9, 1]
+
+// nextInLine([7, 6, 3, 23, 17], 10) ➞ [6, 3, 23, 17, 10]
+
+// nextInLine([1, 10, 20, 42 ], 6) ➞ [10, 20, 42, 6]
+
+// nextInLine([], 6) ➞ "No array has been selected"
+
+const nextInLine = (arr, addition) => {
+    if (arr !== []){
+        arr.shift();
+        arr.push(addition)
+        console.log(arr)
+        return arr
+    }
+    else {
+        return "No array has been selected"
+    }
+}
+
+nextInLine([5,6,7,8,9],1)
